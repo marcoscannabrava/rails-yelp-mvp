3769f52c1cf80d4fa79aa806d50d969f1c63d8bb
Marcos Cannabrava <mpcannabrava@gmail.com>
2019-11-21 07:12:05 -0300

    changed root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'restaurants#index'
   resources :restaurants, only: %i[index show new create] do
     resources :reviews, only: %i[new create]
   end
